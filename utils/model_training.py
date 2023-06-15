ce021b326452cf4035b996caaa370c9891e457b5
aswinram1997 <aswinram@ualberta.ca>
2023-06-15 17:09:47 -0600

    c

--- utils/model_training.py
+++ utils/model_training.py
@@ -14,27 +14,14 @@ def train_xgboost_model(X_train, y_train):
     - model (xgb.XGBClassifier): The trained XGBoost model with the best hyperparameters.
     """
 
-    # Define the parameter grid for random search
-    param_grid = {
-        'max_depth': [3, 6],
-        'learning_rate': [0.1, 0.01],
-        'subsample': [0.6, 1.0],
-        'colsample_bytree': [0.6, 1.0],
-    }
-
     # Create the XGBoost classifier
     model = xgb.XGBClassifier(eval_metric='error', use_label_encoder=False)
 
-    # Create the RandomizedSearchCV object
-    random_search = RandomizedSearchCV(model, param_distributions=param_grid, n_iter=3, cv=3, scoring='f1_macro', random_state=42)
-
-    # Fit the RandomizedSearchCV object on the training data
-    random_search.fit(X_train, y_train)
-
-    # Retrieve the best model
-    model = random_search.best_estimator_
-
+    # Fit the model to the training data
+    model.fit(X_train, y_train)
+    
     return model
+
 
 def calculate_f1_score(model, X_test, y_test):
     """
