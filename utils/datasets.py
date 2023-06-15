f284ed0087998c67cff5933252ce8ad57e685cb9
aswinram1997 <aswinram@ualberta.ca>
2023-06-15 14:04:47 -0600

    commit

--- utils/datasets.py
+++ utils/datasets.py
@@ -3,44 +3,44 @@ import streamlit as st
 
 def load_iris_dataset():
     # Function to load the preprocessed dataset
-    file_path = 'data/Iris.xlsx'
+    file_path = 'C:/Users/aswin/InsightfulClusters/data/Iris.xlsx'
     dataset = pd.read_excel(file_path)
     return dataset
 
 def load_wine_dataset():
     # Function to load the preprocessed dataset
     # Replace with your code to load the dataset
-    file_path = 'data/Wine.xlsx'
+    file_path = 'C:/Users/aswin/InsightfulClusters/data/Wine.xlsx'
     dataset = pd.read_excel(file_path)
     return dataset
 
 def load_mall_customer_dataset():
     # Function to load the preprocessed dataset
-    file_path = 'data/Mall_Customers.xlsx'
+    file_path = 'C:/Users/aswin/InsightfulClusters/data/Mall_Customers.xlsx'
     dataset = pd.read_excel(file_path)
     return dataset
 
 def load_diabetes_dataset():
     # Function to load the preprocessed dataset
-    file_path = 'data/Diabetes.xlsx'
+    file_path = 'C:/Users/aswin/InsightfulClusters/data/Diabetes.xlsx'
     dataset = pd.read_excel(file_path)
     return dataset
 
 def load_wild_blueberry_yield_dataset():
     # Function to load the preprocessed dataset
-    file_path = 'data/WildBlueberry_Yield.xlsx'
+    file_path = 'C:/Users/aswin/InsightfulClusters/data/WildBlueberry_Yield.xlsx'
     dataset = pd.read_excel(file_path)
     return dataset
 
 def load_sleep_health_lifestyle_dataset():
     # Function to load the preprocessed dataset
-    file_path = 'data/Sleep_Health_Lifestyle.xlsx'
+    file_path = 'C:/Users/aswin/InsightfulClusters/data/Sleep_Health_Lifestyle.xlsx'
     dataset = pd.read_excel(file_path)
     return dataset
 
 def load_automobile_customer_segmentation_dataset():
     # Function to load the preprocessed dataset
-    file_path = 'data/Automobile_Customer_Segmentation.xlsx'
+    file_path = 'C:/Users/aswin/InsightfulClusters/data/Automobile_Customer_Segmentation.xlsx'
     dataset = pd.read_excel(file_path)
     return dataset
 
